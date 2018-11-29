318c0f5dd53522e76295ba155e33d4f8be0425f6
Elliot Kim <elliotkim@elliots-mbp.lan>
2018-11-29 00:45:20 -0800

    set up app in react

--- src/client/app.js
+++ src/client/app.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+    this.githubAuth = this.githubAuth.bind(this);
+  }
+
+  componentDidMount() {
+  }
+  githubAuth() {
+    fetch('http://localhost:3000/auth/github', {
+      mode: 'no-cors'
+    })
+      .then(res => {console.log(res)})
+      // .then(data => console.log(data))
+      // .catch(console.error);
+  }
+  
   render() {
     return (
       <div>
         <h1>Hello, World!</h1>
+        <a href='/auth/github'>Sign in with GitHub</a>
+        <input type="submit" value="Sign in with GitHub" onClick={this.githubAuth} />
       </div>
     );
   };
